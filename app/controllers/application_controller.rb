e34a747a0292b1cedd15df669b16e8aba0870596
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-16 15:35:09 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,37 +10,37 @@ class ApplicationController < Sinatra::Base
   end
 
   class Dog
-  
+
       def initialize (name, breed, age)
           @name = name
           @breed = breed
           @age = age
       end
-  
+
   def name
   @name
   end
-  
+
   def name=(name)
   @name = name
   end
-  
+
   def age
   @age
   end
-  
+
   def age=(age)
   @age = age
   end
-  
+
   def breed
   @breed
   end
-  
+
   def breed=(breed)
   @breed = breed
   end
-  
+
   end
 
 
