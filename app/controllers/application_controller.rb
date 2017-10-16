695fe4fd1699fc61f191bb7a403530eef2b4d6c7
Thomas McGlaughlin <tmm86507@gmail.com>
2017-10-16 15:30:48 +0000

    automatically backed up by learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,4 +8,41 @@ class ApplicationController < Sinatra::Base
   get "/" do
   	erb :index
   end
+
+
+end
+
+
+class Dog
+
+    def initialize (name, breed, age)
+        @name = name
+        @breed = breed
+        @age = age
+    end
+
+def name
+@name
+end
+
+def name=(name)
+@name = name
+end
+
+def age
+@age
+end
+
+def age=(age)
+@age = age
+end
+
+def breed
+@breed
+end
+
+def breed=(breed)
+@breed = breed
+end
+
 end
